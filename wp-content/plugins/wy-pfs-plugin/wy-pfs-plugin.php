9bc338ec6017f5df96293ea0c93606f4e39f83ce
jlschillinger <jessica.schillinger@uwyo.edu>
2018-01-30 15:02:55 -0700

    Strategies templates
    single-strategies template, sidebar, creating all the custom fields,
    editing the user roles.

--- wp-content/plugins/wy-pfs-plugin/wy-pfs-plugin.php
+++ wp-content/plugins/wy-pfs-plugin/wy-pfs-plugin.php
@@ -2,9 +2,43 @@
 /*
   Plugin Name: Plugin for WY-PFS Environmental Strategies Catalog
   Description: Site-specific code and functions changes
+  Version: 1.0
+  Date: JULY 2018?????
 */
 
-/* CUSTOM POST TYPES */
+/* =====================================================
+*
+*   TABLE OF CONTENTS
+*
+*   - Notes
+*   - Custom Post TYPES
+*     -- Strategy
+*   - Custom Taxonomies
+*     -- Causal domains
+*     -- Tobacco Goals
+*
+*   =====================================================
+*     GENERAL NOTES ABOUT FEATURES
+*       - Custom Post Types -
+*
+*       There is one custom post type: Strategies. Fields in the Strategies editor are managed with Advanced Custom Fields plugin.
+*
+*       - Custom Taxonomies -
+*
+*       There are two custom taxonomies created for the Strategies custom post type. These are heiarchical, in that they work like categories:
+*       1. Causal Domains
+*       2. Tobacco Goals
+*
+*       ONLY ADMIN can add/edit/delete something from the custom taxonomies. Role/user capabilities are managed using the User Role Editor plugin.
+*
+*  ===================================================== */
+
+
+
+
+/* ============================
+*   CUSTOM POST TYPES
+*  ============================ */
 
 // Create Strategy post type
 function custom_post_type() {
@@ -52,6 +86,7 @@ function custom_post_type() {
         'exclude_from_search' => false,
         'publicly_queryable'  => true,
         'capability_type'     => 'page',
+        'menu_icon'           => 'dashicons-networking' //change the menu icon
     );
 
     // Registering your Custom Post Type
@@ -59,21 +94,19 @@ function custom_post_type() {
 
 }
 
-/* Hook into the 'init' action so that the function
-* Containing our post type registration is not
-* unnecessarily executed.
-*/
-
 add_action( 'init', 'custom_post_type', 0 );
 
 
-/* CUSTOM TAXONOMIES */
+/* ============================
+*   CUSTOM TAXONOMIES
+*  ============================ */
 
-
-if ( ! function_exists( 'custom_taxonomy' ) ) {
+/*  CAUSAL DOMAIN   */
 
 // Register Causal Domain Taxonomy
-function custom_taxonomy() {
+if ( ! function_exists( 'custom_tax_domain' ) ) {
+
+function custom_tax_domain() {
 
 	$labels = array(
 		'name'                       => _x( 'Causal Domains', 'Taxonomy General Name', 'text-domain' ),
@@ -106,11 +139,57 @@ function custom_taxonomy() {
 		'show_in_nav_menus'          => true,
 		'show_tagcloud'              => true,
 	);
-  // REgister the taxonomy, based on the $args, but only for the Strategy Post Type
-	register_taxonomy( 'causal-domain', array( 'strategy' ), $args );
+  // Register the taxonomy, based on the $args, but only for the Strategy Post Type
+	register_taxonomy( 'causal-domain', array( 'strategies' ), $args );
 
 }
-add_action( 'init', 'custom_taxonomy', 0 );
+add_action( 'init', 'custom_tax_domain', 0 );
+
+}
+
+/*  TOBACCO GOALS  */
+
+// Register Tobacco Goal Taxonomy
+if ( ! function_exists( 'custom_tax_goal' ) ) {
+
+function custom_tax_goal() {
+
+	$labels = array(
+		'name'                       => _x( 'Tobacco Goals', 'Taxonomy General Name', 'text-domain' ),
+		'singular_name'              => _x( 'Tobacco Goal', 'Taxonomy Singular Name', 'text-domain' ),
+		'menu_name'                  => __( 'Tobacco Goals', 'text-domain' ),
+		'all_items'                  => __( 'All Tobacco Goals', 'text-domain' ),
+		'parent_item'                => __( 'Parent Tobacco Goals', 'text-domain' ),
+		'parent_item_colon'          => __( 'Parent Tobacco Goal:', 'text-domain' ),
+		'new_item_name'              => __( 'New Tobacco Goal Name', 'text-domain' ),
+		'add_new_item'               => __( 'Add New Tobacco Goal', 'text-domain' ),
+		'edit_item'                  => __( 'Edit Tobacco Goal', 'text-domain' ),
+		'update_item'                => __( 'Update Tobacco Goal', 'text-domain' ),
+		'view_item'                  => __( 'View Tobacco Goal', 'text-domain' ),
+		'separate_items_with_commas' => __( 'Separate tobacco goals with commas', 'text-domain' ),
+		'add_or_remove_items'        => __( 'Add or remove tobacco goals', 'text-domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used tobacco goals', 'text-domain' ),
+		'popular_items'              => __( 'Popular Tobacco Goals', 'text-domain' ),
+		'search_items'               => __( 'Search Tobacco Goals', 'text-domain' ),
+		'not_found'                  => __( 'Not Found', 'text-domain' ),
+		'no_terms'                   => __( 'No tobacco goals', 'text-domain' ),
+		'items_list'                 => __( 'Tobacco goal list', 'text-domain' ),
+		'items_list_navigation'      => __( 'Tobacco goal list navigation', 'text-domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true, // they behave like categories
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+  // Register the taxonomy, based on the $args, but only for the Strategy Post Type
+	register_taxonomy( 'tobacco-goals', array( 'strategies' ), $args );
+
+}
+add_action( 'init', 'custom_tax_goal', 0 );
 
 }
 
---
+++ wp-content/themes/eleanor/sidebar-strategy.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the Strategy Sidebar.
+ * @package: eleanor
+ */
+
+?>
+
+
+<div class="col-md-4 widget-area" id="right-sidebar" role="complementary">
+  <aside class="widget">
+    <h3>Indicator of Effectiveness & Strength of Evidence</h3>
+    <img class="img_harvey" src="images/harvey-icon.png" />
+  </aside>
+  <aside class="widget">
+    <h3>Domain & Goal</h3>
+    <?php the_term('') ?>
+  </aside>
+
+</div>
