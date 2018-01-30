ff685e8e8b3ab77dc29ac8682ba2ac7054ea7ca8
jlschillinger <jessica.schillinger@uwyo.edu>
2018-01-30 15:34:09 -0700

    Making sidebars

--- wp-content/themes/eleanor/sidebar-strategy.php
+++ wp-content/themes/eleanor/sidebar-strategy.php
@@ -7,14 +7,15 @@
 ?>
 
 
-<div class="col-md-4 widget-area" id="right-sidebar" role="complementary">
+<div class="col-md-4 widget-area" id="sidebar-right-strategies" role="complementary">
   <aside class="widget">
     <h3>Indicator of Effectiveness & Strength of Evidence</h3>
     <img class="img_harvey" src="images/harvey-icon.png" />
   </aside>
   <aside class="widget">
     <h3>Domain & Goal</h3>
-    <?php the_term('') ?>
+    <p><?php the_terms( $post->ID, 'causal-domain', 'Causal Domains: ', '/'); ?></p>
+    <p><?php the_terms( $post->ID, 'tobacco-goals', 'Tobacco Goals: ', '/'); ?></p>
   </aside>
 
 </div>
